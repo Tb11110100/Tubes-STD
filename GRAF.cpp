f95637c60f8e706ea40103466b066a14657bad8e
JidanAzizi <idanaf3@gmail.com>
2024-12-27 19:03:15 +0700

    Update GRAF.cpp
    deleteAccount, deleteGroup, unfoll prosedur.

--- GRAF.cpp
+++ GRAF.cpp
@@ -208,3 +208,130 @@ string mostJoined(const Graph& G) {
 
     return mostJoinedGroup;
 }
+
+void deleteAccount(Graph& G, string akun) {
+    // Menghapus akun dari daftar followers setiap akun
+    adrVertexAkun akunToDelete = firstAkun(G);
+    while (akunToDelete != Nil && namaAkun(akunToDelete) != akun) {
+        akunToDelete = nextAkun(akunToDelete);
+    }
+
+    if (akunToDelete != Nil) {
+        // Menghapus follow pada akun lain
+        adrVertexAkun temp = firstAkun(G);
+        while (temp != Nil) {
+            adrEdgeFollow follow = firstFollow(temp);
+            adrEdgeFollow prevFollow = Nil;
+            while (follow != Nil) {
+                if (followName(follow) == akun) {
+                    if (prevFollow == Nil) {
+                        firstFollow(temp) = nextFollow(follow);
+                    } else {
+                        nextFollow(prevFollow) = nextFollow(follow);
+                    }
+                    delete follow;
+                    break;
+                }
+                prevFollow = follow;
+                follow = nextFollow(follow);
+            }
+            temp = nextAkun(temp);
+        }
+
+        // Menghapus akun dari daftar follow
+        adrEdgeFollow follow = firstFollow(akunToDelete);
+        while (follow != Nil) {
+            adrEdgeFollow tempFollow = follow;
+            follow = nextFollow(follow);
+            delete tempFollow;
+        }
+
+        // Menghapus akun dari daftar akun
+        if (firstAkun(G) == akunToDelete) {
+            firstAkun(G) = nextAkun(akunToDelete);
+        } else {
+            adrVertexAkun prevAkun = firstAkun(G);
+            while (nextAkun(prevAkun) != akunToDelete) {
+                prevAkun = nextAkun(prevAkun);
+            }
+            nextAkun(prevAkun) = nextAkun(akunToDelete);
+        }
+        delete akunToDelete;
+    }
+}
+
+void deleteGroup(Graph& G, string grup) {
+    adrVertexGrup grupToDelete = firstGrup(G);
+    while (grupToDelete != Nil && grupName(grupToDelete) != grup) {
+        grupToDelete = nextGrup(grupToDelete);
+    }
+
+    if (grupToDelete != Nil) {
+        // Menghapus semua anggota dari grup
+        adrEdgeMember member = firstMember(grupToDelete);
+        while (member != Nil) {
+            adrEdgeMember tempMember = member;
+            member = nextMember(member);
+            delete tempMember;
+        }
+
+        // Menghapus grup dari daftar grup
+        if (firstGrup(G) == grupToDelete) {
+            firstGrup(G) = nextGrup(grupToDelete);
+        } else {
+            adrVertexGrup prevGrup = firstGrup(G);
+            while (nextGrup(prevGrup) != grupToDelete) {
+                prevGrup = nextGrup(prevGrup);
+            }
+            nextGrup(prevGrup) = nextGrup(grupToDelete);
+        }
+        delete grupToDelete;
+    }
+}
+
+void unfollow(Graph& G, string akun1, string akun2) {
+    adrVertexAkun a1 = firstAkun(G);
+    while (a1 != Nil && namaAkun(a1) != akun1) {
+        a1 = nextAkun(a1);
+    }
+    adrVertexAkun a2 = firstAkun(G);
+    while (a2 != Nil && namaAkun(a2) != akun2) {
+        a2 = nextAkun(a2);
+    }
+
+    if (a1 != Nil && a2 != Nil) {
+        // Menghapus follow dari akun1 ke akun2
+        adrEdgeFollow follow = firstFollow(a1);
+        adrEdgeFollow prevFollow = Nil;
+        while (follow != Nil) {
+            if (followName(follow) == akun2) {
+                if (prevFollow == Nil) {
+                    firstFollow(a1) = nextFollow(follow);
+                } else {
+                    nextFollow(prevFollow) = nextFollow(follow);
+                }
+                delete follow;
+                break;
+            }
+            prevFollow = follow;
+            follow = nextFollow(follow);
+        }
+
+        // Menghapus follow dari akun2 ke akun1 jika mutual
+        follow = firstFollow(a2);
+        prevFollow = Nil;
+        while (follow != Nil) {
+            if (followName(follow) == akun1) {
+                if (prevFollow == Nil) {
+                    firstFollow(a2) = nextFollow(follow);
+                } else {
+                    nextFollow(prevFollow) = nextFollow(follow);
+                }
+                delete follow;
+                break;
+            }
+            prevFollow = follow;
+            follow = nextFollow(follow);
+        }
+    }
+}
